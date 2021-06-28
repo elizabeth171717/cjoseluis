61bed43a76a19400f876ded7265a25aebdc6ede7
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2021-06-27 21:11:33 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const workouts = document.querySelectorAll(".workout-box");
+
+workouts.forEach(function (workout) {
+//console.log(workout)
+const btnss = workout.querySelector(".btn");
+//console.log(btnss);
+btnss.addEventListener("click", function() {
+
+    workouts.forEach(function(item) {
+      if(item !== workout)  {
+          item.classList.remove("show-text");
+      }
+    });
+
+    workout.classList.toggle("show-text");
+});
+});
